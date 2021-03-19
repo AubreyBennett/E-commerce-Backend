aaaf9d5afcc18c4c9390581e0779c972f1b99ea4
Aubrey Bennett <27aubreyb@gmail.com>
2021-03-19 15:12:31 -0400

    added async to my category functions

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 // GET all categories
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 });
 
 // GET a single category by it's id value
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a new category
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await category.create(req.body);
@@ -46,13 +46,13 @@ router.post('/', (req, res) => {
 });
 
 // Update category by its id value
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   
 });
 
 // Delete category by its id value
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
